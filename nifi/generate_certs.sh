884610e5c2b2997212c65d350b2cbf8d99353502
EdmundoHernando <mundi.h.sierra@gmail.com>
2025-01-10 11:53:16 +0100

    Nifi conf

---
+++ nifi/generate_certs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# Crear certificados solo si no existen
+if [ ! -f "./nifi/conf/keystore.p12" ]; then
+  echo "Generando certificados de NiFi..."
+  keytool -genkeypair -alias nifi -keyalg RSA -keysize 2048 \
+    -keystore ./nifi/conf/certs/keystore.p12 -storepass gatvgatv \
+    -keypass gatvgatv -dname "CN=NiFi, OU=Dev, O=Org, L=City, S=State, C=Country" \
+    -storetype PKCS12
+  keytool -exportcert -alias nifi -keystore ./nifi/conf/keystore.p12 \
+    -storepass gatvgatv -file ./nifi/conf/certs/nifi-cert.pem
+else
+  echo "Certificados de NiFi ya existen. No se generarán de nuevo."
+fi
+
+
+if [ ! -f "./nifi/conf/truststore.p12" ]; then
+  echo "Generando TrustStore de NiFi (PKCS12)..."
+  keytool -import -alias nifi-cert \
+    -file ./nifi/conf/nifi-cert.pem \
+    -keystore ./nifi/conf/certs/truststore.p12 \
+    -storepass gatvgatv -storetype PKCS12 -noprompt
+else
+  echo "El TrustStore de NiFi (PKCS12) ya existe. No se generará de nuevo."
+fi
